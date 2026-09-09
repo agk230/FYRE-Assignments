ca6ed1693568289b5ac823e1b3aebe7a7b05476c
Agragyan Khanal <agk230@lehigh.edu>
2026-09-09 15:20:15 -0400

    Add Program3.py to print a name variable

---
+++ Programming Activites/Program3.py
@@ -0,0 +1,7 @@
+# Revision of Program 2 on September 9
+# Print our name with a variable
+
+# variable to store my name
+name = "Agragyan Khanal"
+
+print(name) # output the contents of the variable "name" to the REPL
